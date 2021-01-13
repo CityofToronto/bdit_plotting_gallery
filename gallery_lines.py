ff632854c23bbc0067ce85978170e960ecfaea0d
KatiRG <cnangini@gmail.com>
2021-01-13 11:21:28 -0500

    WIP #2: test docstring format

--- gallery_lines.py
+++ gallery_lines.py
@@ -28,10 +28,36 @@ def lines1(df_lines, sett):
 
 def lines2(df_lines, sett):
     '''Creates another line chart.
-    Parameters
-    -----------
-    df_orig : pandas dataframe
-        Data for the chloropleth map. The data must only contain 2 columns; the first column has to be the geom column and the second has to be the data that needs to be mapped.
     '''
     print("Hello lines2")
     return
+
+def lines3(big_table, keys, other_silly_variable=None):
+    """Fetches rows from a Bigtable.
+
+    Retrieves rows pertaining to the given keys from the Table instance
+    represented by big_table.  Silly things may happen if
+    other_silly_variable is not None.
+
+    Args:
+        big_table: An open Bigtable Table instance.
+        keys: A sequence of strings representing the key of each table row
+            to fetch.
+        other_silly_variable: Another optional variable, that has a much
+            longer name than the other args, and which does nothing.
+
+    Returns:
+        A dict mapping keys to the corresponding table row data
+        fetched. Each row is represented as a tuple of strings. For
+        example:
+
+        {'Serak': ('Rigel VII', 'Preparer'),
+         'Zim': ('Irk', 'Invader'),
+         'Lrrr': ('Omicron Persei 8', 'Emperor')}
+
+        If a key from the keys argument is missing from the dictionary,
+        then that row was not found in the table.
+
+    Raises:
+        IOError: An error occurred accessing the bigtable.Table object.
+    """
