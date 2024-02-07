a417d0f960bbf0fb1feb051ec0d0ae3e56d73e71
Zhangyixue1537 <zhangyixue1537@gmail.com>
2024-02-07 21:54:25 +0000

    #8 plot_line_rick.py

--- sphinx/source/examples/line/plot_line_rick.py
+++ sphinx/source/examples/line/plot_line_rick.py
@@ -4,6 +4,10 @@ RICK Line Chart
 
 Example line chart from the RICK package, with an additional baseline plot and custom formatted x axis.
 """
+import os
+import os.path as osp
+import sys
+sys.path.append(osp.dirname(osp.dirname(osp.dirname(osp.dirname(osp.dirname(osp.abspath(__file__)))))))
 
 from sqlalchemy import create_engine
 import matplotlib.pyplot as plt
@@ -21,80 +25,34 @@ import geopandas as gpd
 import os
 import shapely
 from shapely.geometry import Point
-os.environ["PROJ_LIB"]=r"C:\Users\rliu4\AppData\Local\Continuum\anaconda3\Library\share"
+####os.environ["PROJ_LIB"]=r"C:\Users\rliu4\AppData\Local\Continuum\anaconda3\Library\share"
 import importlib
 import matplotlib.ticker as ticker
 import matplotlib.font_manager as font_manager
 
 
-CONFIG = configparser.ConfigParser()
-# CONFIG.read(r'C:\Users\rliu4\Documents\Python\config.cfg')
-CONFIG.read(r'/home/cnangini/db.cfg')
-dbset = CONFIG['DBSETTINGS']
-con = connect(**dbset)
 ################################
 #Data Collection
 #----------------
 #
-#This Section grabs and formats the data.
+#This Section creates example data.
 
-query='''
-WITH daily_ave AS (
-   SELECT * FROM ptc.daily_trips
-), total AS  (
-SELECT  extract(month from dt) as mon,
-extract(year from dt) as yr,
+# x-axis
+dt=[2015, 2016, 2017, 2018, 2019, 2020]
 
-CASE WHEN extract(year from dt) = 2018 AND extract(month from dt) IN (3) THEN 108321
-WHEN extract(year from dt) = 2018 AND extract(month from dt) IN (11) THEN 161733 ELSE
-avg(count)::integer END as count FROM daily_ave
-GROUP BY extract(month from dt), extract(year from dt)
-ORDER BY extract(year from dt), extract(month from dt)
-)
+# y-axis
+# line 1
+y1=[100, 120, 115, 135, 150, 120]
 
-SELECT 
-CASE WHEN mon = 1 THEN to_char(to_timestamp (mon::text, 'MM'), 'Mon')||' '||yr::text 
-WHEN mon = 9 AND yr = 2016 THEN to_char(to_timestamp (mon::text, 'MM'), 'Mon')||' '||yr::text 
-ELSE to_char(to_timestamp (mon::text, 'MM'), 'Mon')
-END AS period, 
-count FROM total
-'''
-total=pandasql.read_sql(query, con)
+# Create dataframe to be plotted
+data = {'dt':dt, 'y1':y1}
+df_multi = pd.DataFrame(data) 
 
-################################
-#Gets the baseline data (to be graphed in grey)
-query='''
-WITH daily_ave AS (
-SELECT * FROM ptc.daily_trips
-), total AS  (
-SELECT  extract(month from dt) as mon,
-EXTRACT(year from dt) as yr,
-CASE WHEN extract(year from dt) = 2018 AND extract(month from dt) IN (3) THEN 108321
-WHEN extract(year from dt) = 2018 AND extract(month from dt) IN (11) THEN 161733 ELSE
-AVG(count)::integer END AS count FROM daily_ave
-GROUP BY EXTRACT(month from dt), EXTRACT(year from dt)
-ORDER BY EXTRACT(year from dt), EXTRACT(month from dt)
-)
-SELECT 
-CASE WHEN mon = 1 THEN to_char(to_timestamp (mon::text, 'MM'), 'Mon')||' '||yr::text 
-WHEN mon = 9 AND yr = 2016 THEN to_char(to_timestamp (mon::text, 'MM'), 'Mon')||' '||yr::text 
-ELSE to_char(to_timestamp (mon::text, 'MM'), 'Mon')
-END AS period, 
-count/2 AS count FROM total
-'''
-total2=pandasql.read_sql(query, con)
+df_multi_dt = df_multi.set_index('dt')
 
-fig, ax, props = rick.charts.line_chart(total['count'], 'Trips', 'Time', baseline = total2['count'])
-
-################################
-#Adds annotations
-fig.text(0.94, 0.96, '176,000', transform=ax.transAxes, wrap = True, fontsize=9, fontname = 'Libre Franklin',
-         verticalalignment='top', ha = 'center', bbox=props, color = '#660159')
-################################
-#Adds custom x axis
-month_lst2 = ['Sept\n2016',  'Jan\n2017',  'May',  'Sept',
-               'Jan\n2018', 'May',  'Sept', 
-              'Jan\n2019','May',]
-plt.xticks(range(0,35,4), month_lst2, fontsize=9, fontname = 'Libre Franklin')
+fig, ax, props = rick.charts.line_chart(df_multi_dt, xlab='Dates', ylab= "Values", ymin =50, ymax=200)
+fig.tight_layout()
+plt.show()
+plt.savefig("sphinx/source/examples/line/YZtest_line.png")
 
 
