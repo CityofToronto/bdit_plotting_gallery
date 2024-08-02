0b36432d32bbee9d109f1a09f5d2c68338d3cc3d
Raphael Dumas <radumas@users.noreply.github.com>
2024-08-02 12:21:52 -0400

    Upgrade to psycopg3 in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         'matplotlib',
-        'psycopg2',
+        'psycopg[binary]',
         'geopandas',
         'pandas',
         'shapely',
