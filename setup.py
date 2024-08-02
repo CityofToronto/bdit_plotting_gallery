cd329d4ccc6bb0471076a04bdb3cd3f7dc53790c
Raphael Dumas <radumas@users.noreply.github.com>
2024-08-02 12:45:01 -0400

    minor version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rick',
-    version='0.8',
+    version='0.8.1',
     description='Standardized matplotlib charts and graphs',
     packages=find_packages(),
     install_requires=[
